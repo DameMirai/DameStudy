7b22a0de74753a82367939e7ef3675c98037776c
MoonJS <blastingzone@naver.com>
2015-09-13 19:40:56 +0900

    C# Windows ScreenShot Tutorial

---
+++ blastingzone/[Win_C#]ScreenShot/ScreenNetori/ScreenNetori/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace ScreenNetori
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CaptureScreen(object sender, RoutedEventArgs e)
+        {
+            Bitmap bitmap = new Bitmap(Screen.PrimaryScreen.Bounds.Width,
+                                    Screen.PrimaryScreen.Bounds.Height);
+            Graphics graphics = Graphics.FromImage(bitmap as System.Drawing.Image);
+            graphics.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
+            string fileName = @".\" + DateTime.Now.ToString("MMddyyHmmss") + ".jpg";
+            bitmap.Save(fileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+        }
+    }
+
+    
+}
