ea92c80368af0d25b065f7bcc0cb15ab25663133
superhakgoman <flecher1@gmail.com>
2015-09-15 11:03:21 +0900

    regExp 생성시에 i옵션은 case를 ignore한다는 뜻이었다. 위치가 바뀜.. searchAll 추가

--- superhakgoman/chrome_extension/noMoreD/killD.js
+++ superhakgoman/chrome_extension/noMoreD/killD.js
@@ -1,11 +1,25 @@
 String.prototype.replaceAll = function(from, to, caseSensitive){
     var target;
     if (caseSensitive === true) {
-        target = new RegExp(from, 'gi');
-    }else{
         target = new RegExp(from, 'g');
+    }else{
+        target = new RegExp(from, 'gi');
     }
     return this.replace(target, to);
+};
+
+String.prototype.searchAll = function (str, caseSensitive) {
+    var target;
+    if (caseSensitive === true) {
+        target = new RegExp(str, 'g');
+    }else{
+        target = new RegExp(str, 'gi');
+    }
+    var results = [];
+    while (target.exec(this)) {
+        results.push(target.lastIndex - str.length);
+    }
+    return results;
 };
 
 document.body.innerHTML = document.body.innerHTML.replaceAll('됬', '됐', false);
