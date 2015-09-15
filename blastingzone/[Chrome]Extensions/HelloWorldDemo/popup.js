1de2d57e7e85a879a10122287713dc3be0a147de
MoonJS <blastingzone@naver.com>
2015-09-15 21:49:39 +0900

    Chrome Extension JS added

---
+++ blastingzone/[Chrome]Extensions/HelloWorldDemo/popup.js
@@ -0,0 +1,11 @@
+var gameBoard = [];
+
+$(document).ready(function () {
+    createGameBoard();
+    $('#game_board').html("<h2>Change</h2>");
+});
+
+function createGameBoard()
+{
+    console.debug("Call Initial");
+}
