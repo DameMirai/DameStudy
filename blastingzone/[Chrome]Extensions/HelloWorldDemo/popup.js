7cec28048943dd16640368e38835c7f11c4cfdf7
MoonJS <blastingzone@naver.com>
2015-09-17 23:19:09 +0900

    css added

--- blastingzone/[Chrome]Extensions/HelloWorldDemo/popup.js
+++ blastingzone/[Chrome]Extensions/HelloWorldDemo/popup.js
@@ -11,5 +11,24 @@ $(document).ready(function () {
 
 function createGameBoard()
 {
-    $('#game_board').append("AA");
+    var width = $('input[name=board_width_len]').val();
+    if (width === "") width = 5;
+    else width = parseInt(width);
+
+    var height = $('input[name=board_height_len]').val();
+    if (height === "") height = 5;
+    else height = parseInt(height);
+
+    var blankTileHtml = "";
+
+    // create blank board
+    for (var row = 0; row < height; ++row)
+    {
+        for (var col = 0; col < width; ++col)
+        {
+            blankTileHtml += "<div class='gameTile'></div>";
+        }
+    }
+
+    $('#game_board').append(blankTileHtml);
 }
